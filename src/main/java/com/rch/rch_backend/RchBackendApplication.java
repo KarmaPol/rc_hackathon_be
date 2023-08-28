5e5903c5c0cff272f416e6015978cf4ed5660438
Kyunghun Kim <kkhkr98@gmail.com>
2023-08-28 23:43:21 +0900

    feat : first commit

---
+++ src/main/java/com/rch/rch_backend/RchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rch.rch_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RchBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RchBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rch/rch_backend/RchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rch.rch_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RchBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
