8c182bb3447aa4454707c90367f09b9bc1a68c46
boldfaced7 <boldfaced753@gmail.com>
2023-08-29 14:14:58 +0900

    feat : add PostingLike domain

---
+++ src/main/java/com/rch/rch_backend/domain/posting_like/PostingLikeRepository.java
@@ -0,0 +1,6 @@
+package com.rch.rch_backend.domain.posting_like;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostingLikeRepository extends JpaRepository<PostingLike, Long> {
+}
