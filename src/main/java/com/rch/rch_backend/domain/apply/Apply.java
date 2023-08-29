4828012ca773a74ef10e014cb1efc22a1cbb3508
boldfaced7 <boldfaced753@gmail.com>
2023-08-29 14:43:14 +0900

    feat : map Apply and EmployPosting domain

--- src/main/java/com/rch/rch_backend/domain/apply/Apply.java
+++ src/main/java/com/rch/rch_backend/domain/apply/Apply.java
@@ -1,6 +1,7 @@
 package com.rch.rch_backend.domain.apply;
 
 import com.rch.rch_backend.domain.common.BaseEntity;
+import com.rch.rch_backend.domain.employPosting.model.EmployPosting;
 import com.rch.rch_backend.domain.user.model.Users;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,8 +18,8 @@ public class Apply extends BaseEntity {
     @Column(name = "applyId")
     private Long applyId;
     private String phoneNumber;
-    private String resumeFile;
     private String recommender;
+    private String resumeFile;
 
     @Enumerated(EnumType.STRING)
     private ApplyStatus applyStatus;
@@ -27,6 +28,9 @@ public class Apply extends BaseEntity {
     @JoinColumn(name = "userId")
     private Users user;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "posting_id")
+    private EmployPosting employPosting;
     /**
      * 채용 정보 매핑 필요
      */
