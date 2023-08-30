c1427d261ab22bf87e6cd1cd3d410a2e150ab444
boldfaced7 <boldfaced753@gmail.com>
2023-08-30 09:52:43 +0900

    feat : add PostingLikeService

--- src/main/java/com/rch/rch_backend/domain/posting_like/PostingLikeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.rch.rch_backend.domain.posting_like;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostingLikeRepository extends JpaRepository<PostingLike, Long> {
-}
--- src/main/java/com/rch/rch_backend/domain/posting_like/model/PostingLike.java
+++ src/main/java/com/rch/rch_backend/domain/posting_like/model/PostingLike.java
@@ -1,13 +1,18 @@
-package com.rch.rch_backend.domain.posting_like;
+package com.rch.rch_backend.domain.posting_like.model;
 
 import com.rch.rch_backend.domain.common.BaseEntity;
+import com.rch.rch_backend.domain.employPosting.model.EmployPosting;
 import com.rch.rch_backend.domain.user.model.Users;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class PostingLike extends BaseEntity {
@@ -20,4 +25,9 @@ public class PostingLike extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "userId")
     private Users user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "posting_id")
+    private EmployPosting employPosting;
+
 }
